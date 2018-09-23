7895db67086e50ca73a0811cb3cec98a76cdc222
Sergey Lunev <okkam@tutanota.com>
2018-09-23 15:49:30 +0300

    L2.1 refactor

--- L2.1.Memory/L2.1.Memory/src/main/java/ru/otus/l21/Main.java
+++ L2.1.Memory/L2.1.Memory/src/main/java/ru/otus/l21/Main.java
@@ -2,18 +2,8 @@ package ru.otus.l21;
 
 import java.lang.management.ManagementFactory;
 
-/**
- * VM options -Xmx512m -Xms512m
- * <p>
- * Runtime runtime = Runtime.getRuntime();
- * long mem = runtime.totalMemory() - runtime.freeMemory();
- * <p>
- * System.gc()
- * <p>
- * jconsole, connect to pid
- */
-@SuppressWarnings({"RedundantStringConstructorCall", "InfiniteLoopStatement"})
 public class Main {
+
     public static void main(String... args) throws InterruptedException {
         System.out.println("pid: " + ManagementFactory.getRuntimeMXBean().getName());
 
@@ -21,8 +11,20 @@ public class Main {
 
         System.out.println("Starting the loop");
         while (true) {
+            System.gc();
+            Thread.sleep(10);
+
+            Runtime runtime = Runtime.getRuntime();
+            long mem = runtime.totalMemory() - runtime.freeMemory();
+            System.out.println("1st probe: " + mem + " bytes");
+
             Object[] array = new Object[size];
             System.out.println("New array of size: " + array.length + " created");
+
+
+            long mem2 = runtime.totalMemory() - runtime.freeMemory();
+            System.out.println("2nd probe: " + (mem2 - mem) / size + " bytes");
+
             for (int i = 0; i < size; i++) {
                 array[i] = new Object();
                 //array[i] = new String(""); //String pool
@@ -30,12 +32,12 @@ public class Main {
                 //array[i] = new MyClass();
             }
             System.out.println("Created " + size + " objects.");
+
+
+            long mem3 = runtime.totalMemory() - runtime.freeMemory();
+            System.out.println("3rd probe: " + (mem3 - mem2) / size + " bytes");
+
             Thread.sleep(1000); //wait for 1 sec
         }
-    }
-
-    private static class MyClass {
-        private int i = 0;
-        private long l = 1;
     }
 }
